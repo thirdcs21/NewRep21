31c98dc2665c849e505f313f307e07fd4acfcd3a
thirdcs21 <163124869+thirdcs21@users.noreply.github.com>
2024-03-13 11:55:39 +0530

    Update fibo.py

--- fibo.py
+++ fibo.py
@@ -10,13 +10,13 @@ count = 0
 if nterms <= 0:
    print("Please enter a positive integer")
 # if there is only one term, return n1
-elif nterms == 1:
+ nterms == 1:
    print("Fibonacci sequence upto",nterms,":")
    print(n1)
 # generate fibonacci sequence
 else:
    print("Fibonacci sequence:")
-   while count < nterms:
+    count < nterms:
        print(n1)
        nth = n1 + n2
        # update values
